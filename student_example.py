8d78723253c89cf40aa11ca64b5c9a82f714e0c7
Rodolfo De Nadai <rdenadai@ccuec.unicamp.br>
2017-06-26 17:55:18 -0300

    - student.test change to make it possible to keep in memory information about user execution.

--- student_example.py
+++ student_example.py
@@ -4,4 +4,5 @@ from student.test import run_test
 
 
 if __name__ == '__main__':
-    print(run_test('dobro', fnc=lambda n: 2 ** n))
+    print(run_test('dobro.test', fnc=lambda n: 2 ** n))
+    print(run_test('dobro.test', fnc=lambda n: 3 * n))
