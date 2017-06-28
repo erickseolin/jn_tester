79349a7fb6e71328ff0f2a673117b4518ba0a2b5
Erick Seolin <erickseolin@gmail.com>
2017-06-28 14:29:28 -0300

    Testes para TestSet e TestCase

--- tests.py
+++ tests.py
@@ -1,54 +1,108 @@
 from models import TestCase, TestSet
-from student.test import submit_test
+#from student.test import submit_test
 
 
 def equal(x, y):
     return 1.0 if x == y else 0.0
 
 
-def double(b):
-    return 2*b
+def double(n):
+    return 2*n
 
-# Simple test
-test = TestSet()
-test.add_new_test_case(TestCase(2, 4, assert_function=lambda x, y: 1.0 if x == y else 0.0))
-test.add_new_test_case(TestCase({'n': 6}, 12, assert_function=equal))
-test.add_new_test_case(TestCase({'n': 4}, 8, assert_function=equal))
 
-print('1:', test.evaluate(lambda n: 2**n), 'wrong')       # wrong
-print('2:', test.evaluate(double), 'correct')
-print('3:', test.evaluate(lambda n: n**2), 'wrong')      # wrong
-print()
+def test_case_tests():
+    print("starting to test TestCase...")
 
-test.save('dobro.test')
+    # test with lambda functions
+    t = TestCase(2, 4, lambda x, y: 1.0 if x == y else 0.0)
+    assert t.evaluate(lambda x: 2*x) == 1.0
 
-# Load test
-test2 = TestSet()
-test2.load('dobro.test')
+    # test with normal functions
+    t = TestCase(2, 4, equal)
+    assert t.evaluate(double) == 1.0
 
-print(test2)
+    # test with lists
+    _input = [1, 2, 3]
+    _output = [1, 2, 3, 1, 2, 3]
+    t = TestCase(_input, _output, equal)
+    assert t.evaluate(double) == 1.0
 
-print('1:', test2.evaluate(lambda n: 2**n), 'wrong')       # wrong
-print('2:', test2.evaluate(lambda n: 2*n), 'correct')
-print('3:', test2.evaluate(lambda n: n**2), 'wrong')      # wrong
-print()
+    # test false
+    t = TestCase(2, 5, equal)
+    assert t.evaluate(double) == 0.0
 
-# 2 parameter input test
+    # test performance
+    perform = t.performance(double)
+    assert isinstance(perform, dict)
+    assert perform.get('memory', None) is not None
+    assert perform.get('time', None) is not None
 
-test3 = TestSet()
-test3.add_new_test_case(TestCase({'x': 2, 'y': 2}, 4, assert_function=equal))
-test3.add_new_test_case(TestCase({'x': 2, 'y': 3}, 5, assert_function=equal))
-test3.add_new_test_case(TestCase({'x': 10, 'y': 10}, 20, assert_function=equal))
-test3.add_new_test_case(TestCase({'x': 100, 'y': 101}, 201, assert_function=equal))
+    # test multiple input
+    t = TestCase({'x': 6, 'y': 5}, 11, assert_function=equal)
+    assert t.evaluate(lambda x, y: x+y) == 1.0
+    assert t.evaluate(lambda x, y: 3) == 0.0
+    assert t.evaluate(lambda x, y: x*y) == 0.0
+    assert t.evaluate(lambda x, y: 0.0) == 0.0
 
-print('1:', test3.evaluate(lambda x, y: x*y), 'wrong')       # wrong
-print('2:', test3.evaluate(lambda x, y: x+y), 'correct')
-print('3:', test3.evaluate(lambda x, y: x-y), 'wrong')      # wrong
-print()
 
-test3.save('teste3')
+def test_set_tests():
+    print("starting to test TestSet...")
 
-submit_test('teste3', 'rdenadai', lambda x, y: x*y)
-print()
+    test = TestSet()
+    test.add_new_test_case(TestCase(2, 4, lambda x, y: 1.0 if x == y else 0.0))
+    test.add_new_test_case(TestCase({'n': 6}, 12, equal))
+    test.add_new_test_case(TestCase({'n': 4}, 8, equal))
 
-submit_test('teste3', 'rdenadai', lambda x, y: x+y)
+    assert test.evaluate(lambda n: 2**n) == [1.0, 0.0, 0.0]       # wrong
+    assert test.evaluate(double) == [1.0, 1.0, 1.0]
+    assert test.evaluate(lambda n: n**2) == [1.0, 0.0, 0.0]    # wrong
+    assert test.evaluate(lambda n: 3) == [0.0, 0.0, 0.0]    # wrong
+
+    # test save and load
+    test.save('temp.test')
+    test.load('temp.test')
+
+    assert test.evaluate(lambda n: 2**n) == [1.0, 0.0, 0.0]       # wrong
+    assert test.evaluate(double) == [1.0, 1.0, 1.0]
+    assert test.evaluate(lambda n: n**2) == [1.0, 0.0, 0.0]    # wrong
+    assert test.evaluate(lambda n: 3) == [0.0, 0.0, 0.0]    # wrong
+
+
+    # save anoter test
+    test = TestSet()
+    test.add_new_test_case(TestCase(2, 6, equal))
+    test.add_new_test_case(TestCase({'n': 6}, 18, equal))
+
+    assert test.evaluate(lambda n: 3*n) == [1.0, 1.0]
+    assert test.evaluate(lambda n: 4*n) == [0.0, 0.0]
+
+    test.save('temp2')
+
+    # test load previous
+    test = TestSet('temp.test')
+    assert test.evaluate(lambda n: 2**n) == [1.0, 0.0, 0.0]       # wrong
+    assert test.evaluate(double) == [1.0, 1.0, 1.0]
+    assert test.evaluate(lambda n: n**2) == [1.0, 0.0, 0.0]    # wrong
+    assert test.evaluate(lambda n: 3) == [0.0, 0.0, 0.0]    # wrong
+
+    # test load back
+    test = TestSet('temp2.test')
+    assert test.evaluate(lambda n: 3*n) == [1.0, 1.0]
+    assert test.evaluate(lambda n: 4*n) == [0.0, 0.0]
+
+
+if __name__ == '__main__':
+    try:
+        test_case_tests()
+        print("-- TestCase testes passed! --\n")
+        test_set_tests()
+        print("-- TestSet testes passed! --\n")
+    except AssertionError:
+        print("Error ocurred!")
+
+    """
+    submit_test('teste3', 'rdenadai', lambda x, y: x*y)
+    print()
+
+    submit_test('teste3', 'rdenadai', lambda x, y: x+y)
+    """
